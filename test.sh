28637f8a8cc6be279d70039c04de92ffd8ca1ffe
Dcoder99 <deeps.karanji2@gmail.com>
2020-06-25 13:13:22 +0530

    shell commit

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ test -z $repo_name && echo "Repo name required." 1>&2 && exit 1
 curl -u 'DeepikaKaranji:panamastars#2' https://api.github.com/user/repos -d "{\"name\":\"$repo_name\"}"
 git add .
 git commit -m "shell commit"
-git push -u origin master
+git push 'https://DeepikaKaranji:panamastars#2@myrepository.biz/Container-Security-With-JenkinsCI.git' --all 
