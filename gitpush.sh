eb7549db214f3cf9f37f92baf22eb044216ca7fd
Dcoder99 <deeps.karanji2@gmail.com>
2020-06-26 13:58:21 +0530

    added script for gitpush whitesource testing

--- gitpush.sh
+++ gitpush.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 repo_name=$1
 test -z $repo_name && echo "Repo name required." 1>&2 && exit 1
-curl -u 'DeepikaKaranji:panamastars#2' https://api.github.com/user/repos -d "{\"name\":\"$repo_name\"}"
 git add .
-git commit -m "shell commit"
-git push -u origin master
+git commit -m "shell commit test"
+git push
